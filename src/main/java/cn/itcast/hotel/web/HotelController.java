ba428b09e1ba13e5c67dd65c2cfed073e9872510
FFFa-maker <FFFa-maker@outlook.com>
2023-06-05 16:31:41 +0800

    酒店搜索； 添加品牌、城市、星级、价格过滤功能； 添加附近酒店功能

---
+++ src/main/java/cn/itcast/hotel/web/HotelController.java
@@ -0,0 +1,27 @@
+package cn.itcast.hotel.web;
+
+import cn.itcast.hotel.pojo.PageResult;
+import cn.itcast.hotel.pojo.RequestParams;
+import cn.itcast.hotel.service.IHotelService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/hotel")
+public class HotelController {
+
+    @Autowired
+    private IHotelService hotelService;
+
+    @PostMapping("/list")
+    public PageResult search(@RequestBody RequestParams requestParams){
+        return hotelService.search(requestParams);
+    }
+
+//    @PostMapping("/filters")
+//    public PageResult filters(@RequestBody RequestParams requestParams){
+//        return hotelService.search(requestParams);
+//    }
+
+}
